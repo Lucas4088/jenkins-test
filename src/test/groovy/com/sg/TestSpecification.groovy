dad139d502de50c82a7cde0c8778fd47ef2bfecc
Lukasz Salamonik <l.salamonik@partner.samsung.com>
2018-11-05 11:36:26 +0100

    Test1

---
+++ src/test/groovy/com/sg/TestSpecification.groovy
@@ -0,0 +1,14 @@
+package com.sg
+
+import spock.lang.Specification
+
+class TestSpecification extends Specification{
+
+    def "if test then test"() {
+        given:
+
+        when:
+
+        then:
+    }
+}
