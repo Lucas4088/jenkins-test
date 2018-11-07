e2e2060d1377f0e2573c0296c98e2d0446090886
Lukasz Salamonik <l.salamonik@partner.samsung.com>
2018-11-07 14:05:23 +0100

    Fix resources

--- src/main/java/com/sg/core/controller/RickController.java
+++ src/main/java/com/sg/core/controller/RickController.java
@@ -18,8 +18,7 @@ public class RickController {
 
     @GetMapping
     public ResponseEntity<byte[]> getRick() throws IOException {
-        ClassLoader loader = RickController.class.getClassLoader();
-        File gif = new File(Objects.requireNonNull(loader.getResource("media/rick.gif")).getFile());
+        File gif = new File(this.getClass().getResource("media/rick.gif").toString());
 
         return ResponseEntity.ok().contentType(MediaType.IMAGE_GIF)
                 .body(Files.readAllBytes(gif.toPath()));
