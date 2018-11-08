b600730bbc14cc5633f786335cf569ba6efb3d78
Lukasz Salamonik <l.salamonik@partner.samsung.com>
2018-11-08 09:27:38 +0100

    adjust controllers

--- src/main/java/com/sg/core/controller/RickController.java
+++ src/main/java/com/sg/core/controller/RickController.java
@@ -1,29 +1,17 @@
 package com.sg.core.controller;
 
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.ServletContext;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.util.Objects;
 
 @Controller
 @RequestMapping(value = "/rick")
 public class RickController {
 
-    private final ServletContext servletContext;
-
-    public RickController(ServletContext servletContext) {
-        this.servletContext = servletContext;
-    }
 
     @GetMapping
-    public String getRick() throws IOException {
+    public String getRick(){
 
         return "RICK!!!!!!!!!!";
     }
