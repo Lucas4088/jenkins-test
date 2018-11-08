f9e9c679fa62345cfa6a091cf3a21559341d75cd
Lukasz Salamonik <l.salamonik@partner.samsung.com>
2018-11-08 09:37:21 +0100

    adjust web config

--- src/main/java/com/sg/Config.java
+++ src/main/java/com/sg/Config.java
@@ -1,10 +1,18 @@
 package com.sg;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 
 @EnableWebMvc
+@Configuration
+@ComponentScan
 public class Config implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/media/**");
+    }
 }
